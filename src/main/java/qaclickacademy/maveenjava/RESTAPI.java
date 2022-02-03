23ace4a40405c1427770bee06616ca8d6c38ab08
Keerthi <v.keerthi29@gmail.com>
2022-02-03 09:40:26 -0500

    first commit

---
+++ src/main/java/qaclickacademy/maveenjava/RESTAPI.java
@@ -0,0 +1,18 @@
+package qaclickacademy.maveenjava;
+
+import org.testng.annotations.Test;
+
+public class RESTAPI {
+	
+	@Test
+	public void postjira()
+	{
+	System.out.println("postjira");
+	}
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+	}
+
+}
---
+++ src/main/java/qaclickacademy/maveenjava/SeleniumTest.java
@@ -0,0 +1,21 @@
+package qaclickacademy.maveenjava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	
+	//testng xml file from maven
+	//how to execute all test cases from Test folder - mvn test
+	@Test
+	public void BrowserAutomation()
+	{
+	  System.out.println("BrowserAutomation");
+	}
+	@Test
+	public void elements()
+	{
+		System.out.println("elements");
+	}
+
+}
